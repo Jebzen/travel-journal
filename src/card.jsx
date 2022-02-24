702b85040f4f0f8043ce680122a1fb8ab8db8165
Jeppe Bentzen <jeb.bentzen@gmail.com>
2022-02-24 11:47:23 +0100

    Checkpoint 2

--- src/card.jsx
+++ src/card.jsx
@@ -2,19 +2,21 @@ import './card.css';
 import Picture from './picture';
 
 const Card = (props)=>{
-    console.log(props);
+    //console.log(props);
     const Class = `card ${props.destination}`
 
     return(
         <div className={Class}>
             <Picture destination={props.destination}/>
             <div>
-                <div>
-                    {props.destination}
-                    {props.maps}
+                <div className='line'>
+                    <h3><i className='bi bi-geo-alt-fill'></i>{props.destination}</h3>
+                    <p className='link'>
+                        <a href={props.maps} target="_blank" rel="noopener noreferrer">View On Google Maps</a>
+                    </p>
                 </div>
                 <h1>{props.attraction}</h1>
-                <p>{props.dates}</p>
+                <p className='dates'>{props.dates}</p>
                 <p>{props.desc}</p>
             </div>
         </div>
--- src/content.jsx
+++ src/content.jsx
@@ -8,8 +8,21 @@ function Content(){
             attraction: "Mount Fuji",
             dates: "12 Jan, 2021 - 24 Jan, 2021",
             maps: "###",
-            picture: "japan.jpg",
             desc: "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        },
+        {
+            destination: "Australia",
+            attraction: "Sydney Opera House",
+            dates: "12 Jan, 2021 - 24 Jan, 2021",
+            maps: "###",
+            desc: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings"
+        },
+        {
+            destination: "Norway",
+            attraction: "Geirangerfjord",
+            dates: "12 Jan, 2021 - 24 Jan, 2021",
+            maps: "###",
+            desc: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality."
         }
     ]
     return (
